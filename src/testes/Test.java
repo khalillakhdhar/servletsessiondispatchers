1732285912249075eb22b66c9ef87c2cc94b1f67
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-27 13:15:36 +0100

    Create Test.java

---
+++ src/testes/Test.java
@@ -0,0 +1,13 @@
+package testes;
+import classes.User;
+import classes.Users;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+User u1=new User("teste","teste","teste",12);
+Users us=new Users();
+
+	}
+
+}
