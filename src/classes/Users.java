a3af539d52bee65acca477ca07dbd4fd3edd673e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-27 13:00:43 +0100

    Create Users.java

---
+++ src/classes/Users.java
@@ -0,0 +1,10 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class Users {
+	public static ArrayList<User> mylist=new ArrayList<User>(); //initialisation
+	public void adduser()
+	{}
+
+}
