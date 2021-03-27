f5933b347b089436f00d62920c0955d580b8bb05
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-27 13:48:15 +0100

    testes

--- src/testes/Test.java
+++ src/testes/Test.java
@@ -7,7 +7,11 @@ public class Test {
 		// TODO Auto-generated method stub
 User u1=new User("teste","teste","teste",12);
 Users us=new Users();
-
+us.adduser(u1);
+String t1=us.exists("teste","teste").toString();
+User t2=us.exists("a","b");
+System.out.println("le résultat du premier teste est "+t1);
+System.out.println("le résultat du deuxiéme teste est "+t2);
 	}
 
 }
